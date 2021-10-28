fce9d6494d24732e8703d0634548e6b6957118d9
Patrik Nygren <patriknygren82@gmail.com>
2021-10-28 14:10:23 +0200

    async await syntax and separation of app and db

--- index.js
+++ index.js
@@ -1,81 +1,96 @@
 import express from "express"
 import { MongoClient, ObjectId } from "mongodb"
 
-const dbName = 'stundentsexample';
-const url = `mongodb://localhost:27017/${dbName}`;
+const dbConf = {
+  name: 'stundentsexample',
+  connectionUrl: `mongodb://localhost:27017/stundentsexample`
+}
 
 const app = express()
-const port = 3000
+const port = process.env.PORT; // 3000 (development) or 80 (production)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-MongoClient.connect(url, function (err, database) {
-  if (err) {
-    console.error("Connection error", err)
-  }
-  console.log("Connected successfully to server");
-  const db = database.db(dbName);
+// TODO refactor
+// 1. configure object for db stuff [x]
+// 2. separate app and db (inject) [x]
+// 3. separate app and db in modules
+// 4. create mock db
 
-  // GET /students => retrieves all students in db
-  app.get('/students', (req, res) => {
-    db.collection('students')
-      .find({})
-      .toArray((err, students) => {
-        if (err) {
-          console.error("error GET /students", err)
-          res.send(err)
-        } else {
-          res.send(students)
-        }
-      })
+const connectToDb = async (dbConf) => {
+  try {
+    const client = await MongoClient.connect(dbConf.connectionUrl)
+    const db = client.db(dbConf.name);
+    console.log("Connected successfully to db");
+    return db;
+  } catch (error) {
+    console.error("DB Connection error", error);
+    throw error;
+  }
+}
+
+const createApp = async (db) => {
+  app.get('/students', async (req, res) => {
+    try {
+      const students = await db.collection('students').find({}).toArray()
+      res.send(students)
+    } catch (err) {
+      console.error("Error GET /students", err)
+      res.statusCode = 501
+      res.send("server error")
+    }
   })
 
   // GET /students/id => retrieves one student with id
-  app.get('/students/:id', (req, res) => {
+  app.get('/students/:id', async (req, res) => {
     const id = req.params.id
-    db.collection('students')
-      .findOne({ "_id": ObjectId(id) }, (err, student_doc) => {
-        if (err) {
-          res.send(err)
-        } else {
-          res.send(student_doc)
-        }
-      })
+    try {
+      const student = await db.collection('students').findOne({ "_id": ObjectId(id) })
+      res.send(student)
+    } catch (err) {
+      console.error("Error GET /students/id", err)
+      res.status(501).send("server error")
+    }
   })
 
   // POST /students { name: "Petter", age: 17 } => saves the student in db
-  app.post('/students', (req, res) => {
+  app.post('/students', async (req, res) => {
     const { name, age } = req.body
-    db.collection('students')
-      .insertOne({ name, age }, (err, obj) => {
-        if (err) {
-          res.send(err)
-        } else {
-          res.send(`succeful insert of object ${obj.insertedId}`)
-        }
-      })
+    try {
+      await db.collection('students').insertOne({ name, age })
+      res.status(201).send("created")
+    } catch (err) {
+      console.error("Error POST /students", err)
+      res.status(501).send("server error")
+    }
   })
 
   // DELETE /students/id => deletes student with id
-  app.delete('/students/:id', (req, res) => {
+  app.delete('/students/:id', async (req, res) => {
     const id = req.params.id
-    console.log("id", id)
-    db.collection('students')
-      .deleteOne({ "_id": ObjectId(id) }, (err, obj) => {
-        if (err) {
-          res.send(err)
-        } else {
-          res.send(`successful deletion of ${obj.deletedCount} documents`)
-        }
-      })
+    try {
+      await db.collection('students').deleteOne({ "_id": ObjectId(id) })
+      res.status(204).send("deleted")
+    } catch (err) {
+      console.error("Error POST /students", err)
+      res.status(501).send("server error")
+    }
   })
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+  return app;
+}
 
-});
+const main = async (port, dbConf) => {
+  try {
+    const db = await connectToDb(dbConf);
+    const app = await createApp(db)
+    app.listen(port, () => {
+      console.log(`Students app listening at http://localhost:${port}`)
+    })
+  } catch (err) {
+    console.error("Error running app", err)
+  }
+}
 
-
-
+main(port, dbConf);
