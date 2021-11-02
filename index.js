3601e9465ccb115612e8626852d1d7772a59b6fe
Patrik Nygren <patriknygren82@gmail.com>
2021-11-02 14:34:08 +0100

    config environment variables in azure

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 import createDb from "./storage/db.js"
 import createApp from "./app.js"
 
-const dbConf = {
-  name: 'stundentsexample',
-  connectionUrl: `mongodb+srv://patrik:123456qwerty@studentsexample.hkn0m.mongodb.net/studentsexample?retryWrites=true&w=majority`
-}
+// Config
+const APP_ENV = process.env.APP_ENV || "dev"
+const PORT = process.env.PORT || 3000
+const DB_TYPE = process.env.DB_TYPE || "mock"
+const DB_CONN = process.env.DB_CONN || "<default>"
+const DB_NAME = process.env.DB_NAME || "<default>"
 
-const port = process.env.PORT; // 3000 or 80
-const dbType = process.env.DB; // mock or mongo
-
-const main = async (port, dbConf) => {
+const main = async () => {
   try {
-    const db = await createDb(dbConf, dbType);
+    const db = await createDb(DB_TYPE, DB_CONN, DB_NAME);
     const app = await createApp(db)
-    app.listen(port, () => {
-      console.log(`Students app (${dbType}) listening at http://localhost:${port}`)
+    app.listen(PORT, () => {
+      console.log(`Students app (${APP_ENV}) with DB (${DB_TYPE}) listening at ${PORT}`)
     })
   } catch (err) {
     console.error("Error running app", err)
   }
 }
 
-main(port, dbConf);
+main();
