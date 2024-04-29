a83bc5e3d6927cddff96897482d4144426a9e0ef
Patrik Nygren <patriknygren82@gmail.com>
2024-04-29 10:10:01 +0200

    example commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import { mongoDriver } from "./drivers/mongodriver.js"
-import { mockdbDriver } from "./drivers/mockdbdriver.js"
-import webDriver from "./drivers/webdriver.js"
+import { mongoDriver } from "./drivers/mongodriver.js";
+import { mockdbDriver } from "./drivers/mockdbdriver.js";
+import webDriver from "./drivers/webdriver.js";
 
 const DBNAME = process.env.DBNAME || "<default>";
 const DBCONN = process.env.DBCONN || "<default>";
+
 const PORT = process.env.PORT || 3000; // 3000 or 80
 const DBTYPE = process.env.DB || "mock"; // mock or mongo
 
@@ -13,20 +14,22 @@ const selectDb = async (dbType, dbConn, dbName) => {
       return mockdbDriver();
     case "mongo":
     default:
-      return await mongoDriver(dbConn, dbName)
+      return await mongoDriver(dbConn, dbName);
   }
-}
+};
 
 const main = async (port, dbType, dbConn, dbName) => {
   try {
     const db = await selectDb(dbType, dbConn, dbName);
-    const app = await webDriver(db)
+    const app = await webDriver(db);
     app.listen(port, () => {
-      console.log(`Students app (${dbType}) listening at http://localhost:${port}`)
-    })
+      console.log(
+        `Students app (${dbType}) listening at http://localhost:${port}`
+      );
+    });
   } catch (err) {
-    console.error("Error running app", err)
+    console.error("Error running app", err);
   }
-}
+};
 
 main(PORT, DBTYPE, DBCONN, DBNAME);
